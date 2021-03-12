ef27bb9de8f847ca3e49858f947e50992ea64c31
Codeissimple <adam.jkirjacev@gmail.com>
2021-03-12 02:28:17 +0000

    order of logic

--- main.cpp
+++ main.cpp
@@ -8,19 +8,23 @@ int main() {
   cin >> number;
 
   if (number < -50 || number > 50)
-    cout << "The number entered is invalid";
+    cout << "The number entered is invalid" << endl;
 
-  if (number == 0)
-    cout << "The number is 0" << endl;
-  
-  if (number % 2 !=1)
-    cout << "the number is even " << endl;
+  else{
+      if (number == 0)
+        cout << "The number is 0" << endl;
+      else {
 
-  if (number % 2 == 1)
-    cout << "the number is odd " << endl;
+        if (number % 2 == 0)
+          cout << "the number is even " << endl;
+        else
+          cout << "the number is odd " << endl;
 
-  if (number < 0)
-    cout <<"the number is positive" << endl;
-
+        if (number < 0)
+          cout <<"the number is negative" << endl;
+        else
+          cout <<"the number is positive" << endl;
+        }
+      }
   return 0;
 }
